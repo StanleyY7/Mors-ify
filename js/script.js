295d7b78f4ef573aef1d7009668e2ce0e19bccd7
StanleyY7 <stanleyyu7@gmail.com>
2023-01-11 21:03:32 +1100

    iteration 2, 25% complete, text to morse working

--- js/script.js
+++ js/script.js
@@ -4,20 +4,162 @@ window.addEventListener("DOMContentLoaded", () => {
   const morseSubmit = document.querySelector("#morse-submit");
   const closeIcon = document.querySelector("#close");
   const outputContainer = document.getElementById("output-container");
+  const body = document.querySelector("body");
+
+  const mapping = {
+    A: ".-",
+    B: "-...",
+    C: "-.-.",
+    D: "-..",
+    E: ".",
+    F: "..-.",
+    G: "--.",
+    H: "....",
+    I: "..",
+    J: ".---",
+    K: "-.-",
+    L: ".-..",
+    M: "--",
+    N: "-.",
+    O: "---",
+    P: ".--.",
+    Q: "--.-",
+    R: ".-.",
+    S: "...",
+    T: "-",
+    U: "..-",
+    W: ".--",
+    X: "-..-",
+    Y: "-.--",
+    Z: "--..",
+
+    0: "-----",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+  };
+
+  const opposite = {
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+
+    "-----": 0,
+    ".----": 1,
+    "..---": 2,
+    "...--": 3,
+    "....-": 4,
+    ".....": 5,
+    "-....": 6,
+    "--...": 7,
+    "---..": 8,
+    "----.": 9,
+  };
+
+  /* Functions */
+
+  let textToMorse = () => {
+    outputContainer.style.display = "block";
+    let input = document.getElementById("input-text").value.toUpperCase();
+
+    let arr1 = input.split("");
+
+    let arr2 = arr1.map((x) => {
+      if (mapping[x]) {
+        return mapping[x];
+      } else {
+        return x;
+      }
+    });
+
+    let code = arr2.join(" ");
+
+    document.getElementById("output").value = code;
+  };
+
+  let morseToText = () => {
+    outputContainer.style.display = "block";
+    let trueInput = document.getElementById("input-morse").value;
+    let trueInputArr = [trueInput];
+    let input = trueInput.toUpperCase();
+
+    let morseArr = input.split("");
+
+    let mappedMorseArr = morseArr.map((y) => {
+      if (opposite[y]) {
+        return opposite[y];
+      } else {
+        return trueInputArr[trueInputArr.length - 1];
+      }
+    });
+
+    let codeMorse = mappedMorseArr.join(" ");
+    document.getElementById("output").value = codeMorse;
+  };
 
   const hideOutput = () => {
     outputContainer.style.display = "none";
   };
 
-  const displayOutput = () => {
-    outputContainer.style.display = "block";
+  const displayOutputText = () => {
+    textToMorse();
+  };
+
+  const displayOutputMorse = () => {
+    morseToText();
   };
 
   closeIcon.addEventListener("click", hideOutput);
-  textSubmit.addEventListener("click", displayOutput);
-  morseSubmit.addEventListener("click", displayOutput);
+  textSubmit.addEventListener("click", displayOutputText);
+  morseSubmit.addEventListener("click", displayOutputMorse);
 
-  // Draggable Output
+  /* Clara Intro */
+
+  const claraStart = document.querySelector("#clara-start");
+  const claraNext = document.querySelector("#clara-next");
+  const startClara = (event) => {
+    if (event.key === "Enter") {
+      claraStart.style.display = "none";
+      claraNext.style.display = "block";
+      body.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") claraNext.style.display = "none";
+      });
+    } else if (event.key === "s") {
+      claraStart.remove();
+    }
+  };
+
+  body.addEventListener("keypress", startClara);
+
+  /* Draggable Output Container*/
 
   const draggableElements = document.querySelectorAll(".draggable");
 
